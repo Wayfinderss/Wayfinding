fd19410af307037fcc5ac11e5a0a83daf8a9df28
KUS30 <KUS30@pitt.edu>
2026-03-02 22:18:37 -0500

    Fixed Main

--- app/main.py
+++ app/main.py
@@ -1,10 +1,12 @@
 from fastapi import FastAPI
 
 from app.api.routes import router
+from app.api.demand import router as demand_router
 
 def create_app():
     app = FastAPI()
     app.include_router(router)
+    app.include_router(demand_router)
     return app
 
 app = create_app()
