d0b8bf4f911bec8e9a84e560d74a609fa34a8d53
Jessica <jhb59@pitt.edu>
2026-04-08 23:17:42 -0400

    sidebar's fixed

--- wayfinder/domain/geojson_to_osm_service.py
+++ wayfinder/domain/geojson_to_osm_service.py
@@ -92,13 +92,13 @@ def tags_from_properties(attrs: dict) -> dict:
     if grade:
         tags["incline"] = f"{grade}%"
 
-            # accessibility hint
-            if grade <= 5:
-                tags["wheelchair"] = "yes"
-            elif grade <= 8:
-                tags["wheelchair"] = "limited"
-            else:
-                tags["wheelchair"] = "no"
+        # accessibility hint
+        if grade <= 5:
+            tags["wheelchair"] = "yes"
+        elif grade <= 8:
+            tags["wheelchair"] = "limited"
+        else:
+            tags["wheelchair"] = "no"
 
         # ----- Road name -----
         road = props.get("RoadName")
